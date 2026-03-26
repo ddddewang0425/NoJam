e3741ca474959b66439eb68359181b9cefb779c0
ddddewang0425 <ddddewang0425@gmail.com>
2026-03-26 15:58:00 +0900

    asdf

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import { Outlet, useLocation } from 'react-router-dom'
 import Sidebar from './Sidebar'
 import BottomNav from './BottomNav'
 import useTaskStore from '../store/taskStore'
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Layout() {
   const location = useLocation()
